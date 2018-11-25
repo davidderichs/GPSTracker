07d91ad5181941e485d7b9b9bfae803aa260b3d5
s57343 <s57343@beuth-hochschule.de>
2018-11-25 15:42:28 +0100

    Save to GPX and Route-Painting implemented.

--- app/src/main/java/davidderichs/gpstracker/MainActivity.java
+++ app/src/main/java/davidderichs/gpstracker/MainActivity.java
@@ -27,18 +27,20 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import com.ramotion.fluidslider.FluidSlider;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.text.DecimalFormat;
+import java.util.Iterator;
 import java.util.LinkedList;
-
 import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, LocationListener, SensorEventListener {
@@ -71,13 +73,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     float height;
     float height_offset;
 
-    LinkedList<String> route = new LinkedList<>();
-    LinkedList<String> routeUTM = new LinkedList<>();
+    LinkedList<String> GPS_Route = new LinkedList<String>();
+    LinkedList<UTMRef> UTM_Route = new LinkedList<UTMRef>();
 
     private final int ACCESS_FINE_LOCATION=1;
 
-    Button button_SwitchToGPS;
-    Button button_GetHeight;
+    Button button_saveRouteToGPX;
     Button button_resetCalibration;
     FluidSlider slider_SetNewCalibratedHeight;
 
@@ -95,8 +96,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         showPhoneStatePermissionGPS();
 
-        this.route.clear();
-        this.routeUTM.clear();
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(this.openFileOutput("GPSTracks.csv", Context.MODE_PRIVATE));
+            outputStreamWriter.write("");
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.GPS_Route.clear();
+        this.UTM_Route.clear();
 
         height_offset = 0.0f;
         calibrationActive = false;
@@ -126,11 +137,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //            Log.d("GPSReceiver", e.getMessage());
         }
 
-        button_SwitchToGPS = (Button) findViewById(R.id.getGPS);
-        button_SwitchToGPS.setOnClickListener(this);
-
-        button_GetHeight = (Button) findViewById(R.id.getPressure);
-        button_GetHeight.setOnClickListener(this);
+        button_saveRouteToGPX = (Button) findViewById(R.id.button_saveRouteToGPS);
+        button_saveRouteToGPX.setOnClickListener(this);
 
         button_resetCalibration = (Button) findViewById(R.id.resetCalibration);
         button_resetCalibration.setOnClickListener(this);
@@ -183,13 +191,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void saveGPSInformationToFile(Location location){
         try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(this.openFileOutput("GPSTracks.csv", Context.MODE_PRIVATE));
+
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(this.openFileOutput("GPSTracks.csv", Context.MODE_APPEND));
 
             outputStreamWriter.write(
-                    location.getLongitude() +
-                    "," + location.getLatitude() +
+                    location.getLatitude() +
+                    "," + location.getLongitude() +
                     "," + location.getSpeed() +
                     ";");
+            outputStreamWriter.write(System.getProperty("line.separator"));
             outputStreamWriter.close();
         }
         catch (IOException e) {
@@ -209,7 +219,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 StringBuilder stringBuilder = new StringBuilder();
 
                 while ( (receiveString = bufferedReader.readLine()) != null ) {
-                    this.route.add(receiveString);
+                    this.GPS_Route.add(receiveString);
                 }
 
                 inputStream.close();
@@ -304,6 +314,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void saveRouteToGPX() {
+        String header = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?><gpx xmlns=\"http://www.topografix.com/GPX/1/1\" creator=\"MapSource 6.15.5\" version=\"1.1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"  xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\"><trk>\n";
+        String name = "<name>" + "" + "</name><trkseg>\n";
+
+        String segments = "";
+
+        Iterator routeIterator = this.GPS_Route.iterator();
+        while (routeIterator.hasNext()){
+            String routeItem = (String) routeIterator.next();
+            String[] routeInfo = routeItem.split(",");
+            String latitude = routeInfo[0];
+            String longitude = routeInfo[1];
+            segments += "<trkpt lat=\"" + latitude + "\" lon=\"" + longitude + "\"></trkpt>\n";
+        }
+
+        String footer = "</trkseg></trk></gpx>";
+
+
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(this.openFileOutput("Route.gpx", Context.MODE_PRIVATE));
+            outputStreamWriter.append(header);
+            outputStreamWriter.append(name);
+            outputStreamWriter.append(segments);
+            outputStreamWriter.append(footer);
+            outputStreamWriter.flush();
+            outputStreamWriter.close();
+            Log.d("GPSReceiver", "Saved to GPX");
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("GPSReceiver", "Error Writting Path: " + e.getMessage());
+        }
+    }
+
 
     @SuppressLint("MissingPermission")
     private void getGPSData() {
@@ -321,13 +366,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.getGPS:
-//                Log.d("GPSReceiver", "Switching to GPS");
-                getGPSData();
-                break;
-            case R.id.getPressure:
-//                Log.d("GPSReceiver", "Getting Pressure Data");
-                getHeightViaPressure();
+            case R.id.button_saveRouteToGPS:
+                saveRouteToGPX();
                 break;
             case R.id.action_route:
                 navigationRouteClicked();
@@ -395,7 +435,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             linearLayout_Calibration.setVisibility(View.GONE);
             linearLayout_standard_content.setVisibility(View.GONE);
             drawCanvas();
-            drawRoute();
 
         } else {
             linearLayout_Settings.setVisibility(View.GONE);
@@ -404,14 +443,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void convertGPSRouteToUTMRoute(){
+        Iterator routeIterator = this.GPS_Route.iterator();
+        while (routeIterator.hasNext()){
+            String routeItem = (String) routeIterator.next();
+            String[] routeInfo = routeItem.split(",");
 
+            LatLng gps = new LatLng( new Double(routeInfo[0]), new Double (routeInfo[1]) );
+
+            UTMRef utm = gps.toUTMRef();
+
+            this.UTM_Route.add(utm);
+        }
     }
 
-    private void drawRoute(){
-        Log.d("GPSReceiver", "Drawing Route on Canvas");
-        for(String routeEntry : this.route){
-            Log.d("GPSReceiver", routeEntry);
+    private double getMaxEastFromUTMRoute(){
+        Iterator it = UTM_Route.iterator();
+
+        double maxValue = Double.MIN_VALUE;
+
+        while (it.hasNext()){
+            UTMRef routeEntry = (UTMRef) it.next();
+            if(routeEntry.getEasting()>maxValue){
+                maxValue = routeEntry.getEasting();
+            }
         }
+
+        return maxValue;
+    }
+
+    private double getMinEastFromUTMRoute(){
+        Iterator it = UTM_Route.iterator();
+
+        double minValue = Double.MAX_VALUE;
+
+        while (it.hasNext()){
+            UTMRef routeEntry = (UTMRef) it.next();
+            if(routeEntry.getEasting()<minValue){
+                minValue = routeEntry.getEasting();
+            }
+        }
+
+        return minValue;
+    }
+
+    private double getMaxNorthFromUTMRoute(){
+        Iterator it = UTM_Route.iterator();
+        double maxValue = Double.MIN_VALUE;
+        while (it.hasNext()){
+            UTMRef routeEntry = (UTMRef) it.next();
+            if(routeEntry.getNorthing()>maxValue){
+                maxValue = routeEntry.getNorthing();
+            }
+        }
+        return maxValue;
+    }
+
+    private double getMinNorthFromUTMRoute(){
+        Iterator it = UTM_Route.iterator();
+        double minValue = Double.MAX_VALUE;
+        while (it.hasNext()){
+            UTMRef routeEntry = (UTMRef) it.next();
+            if(routeEntry.getNorthing()<minValue){
+                minValue = routeEntry.getNorthing();
+            }
+        }
+        return minValue;
     }
 
     private void drawCanvas() {
@@ -423,10 +519,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             Canvas canvas = new Canvas(bitmap);
             Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            paint.setColor(Color.GRAY);
-            canvas.drawLine(quarterWidth, 0, quarterWidth, bitmapHeight, paint);
+
+            paint.setColor(Color.BLUE);
+
+            double maxEastUTMValue = getMaxEastFromUTMRoute();
+            double minEastUTMValue = getMinEastFromUTMRoute();
+
+            double maxNorthUTMValue = getMaxNorthFromUTMRoute();
+            double minNorthUTMValue = getMinNorthFromUTMRoute();
+
+            int longZone;
+            char latZone;
+
+            if (this.UTM_Route.size() == 0){
+                return;
+            }else if(this.UTM_Route.size() < 2){
+                Iterator it = this.UTM_Route.iterator();
+                UTMRef routeEntry = (UTMRef) it.next();
+
+                longZone = routeEntry.getLngZone();
+                latZone = routeEntry.getLatZone();
+
+                double xPos1;
+                double yPos1;
+
+                xPos1 = ( (maxEastUTMValue-minEastUTMValue) / (routeEntry.getEasting()*bitmapWidth) );
+                yPos1 = ( (maxNorthUTMValue-minNorthUTMValue) / (routeEntry.getNorthing()*bitmapHeight) );
+
+                float xPos1Canvas = (float) xPos1 * bitmapWidth;
+                float yPos1Canvas = (float) yPos1 * bitmapHeight;
+
+                canvas.drawLine(xPos1Canvas, yPos1Canvas, xPos1Canvas, yPos1Canvas, paint);
+                canvas.drawText((latZone+Integer.toString(longZone)), 10, 25, paint);
+            } else {
+                double xPos1;
+                double yPos1;
+                double xPos2;
+                double yPos2;
+
+                Iterator it = this.UTM_Route.iterator();
+                UTMRef routeEntry = (UTMRef) it.next();
+
+                longZone = routeEntry.getLngZone();
+                latZone = routeEntry.getLatZone();
+
+                xPos1 = ( ( (routeEntry.getEasting() - minEastUTMValue) ) / (maxEastUTMValue-minEastUTMValue) );
+                yPos1 = ( ( (routeEntry.getNorthing() - minNorthUTMValue) ) / (maxNorthUTMValue-minNorthUTMValue) );
+                while(it.hasNext()){
+                    routeEntry = (UTMRef) it.next();
+                    xPos2 = ( ( (routeEntry.getEasting() - minEastUTMValue) ) / (maxEastUTMValue-minEastUTMValue) );
+                    yPos2 = ( ( (routeEntry.getNorthing() - minNorthUTMValue) ) / (maxNorthUTMValue-minNorthUTMValue) );
+
+                    float xPos1Canvas = (float) xPos1 * bitmapWidth;
+                    float yPos1Canvas = (float) yPos1 * bitmapHeight;
+
+                    float xPos2Canvas = (float) xPos2 * bitmapWidth;
+                    float yPos2Canvas = (float) yPos2 * bitmapHeight;
+
+                    Log.d("GPSReceiver", "xpos1: " + xPos1Canvas);
+                    Log.d("GPSReceiver", "Northing: " + routeEntry.getNorthing());
+                    Log.d("GPSReceiver", "ypos1: " + yPos1Canvas);
+                    Log.d("GPSReceiver", "xpos2: " + xPos2Canvas);
+                    Log.d("GPSReceiver", "ypos2: " + yPos2Canvas);
+
+                    canvas.drawLine(xPos1Canvas, yPos1Canvas, xPos2Canvas, yPos2Canvas, paint);
+
+                    xPos1 = xPos2;
+                    yPos1 = yPos2;
+                }
+            }
+
+            paint.setColor(Color.RED);
+//            canvas.drawLine(quarterWidth, 0, quarterWidth, bitmapHeight, paint);
             canvas.drawLine(quarterWidth*2, 0, quarterWidth*2, bitmapHeight, paint);
-            canvas.drawLine(quarterWidth*3, 0, quarterWidth*3, bitmapHeight, paint);
+            paint.setTextSize(50);
+            canvas.drawText((latZone+Integer.toString(longZone)), quarterWidth*2, 0, paint);
+//            canvas.drawLine(quarterWidth*3, 0, quarterWidth*3, bitmapHeight, paint);
 
             imageView_Route.draw(canvas);
             imageView_Route.setImageBitmap(bitmap);
@@ -455,24 +623,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onLocationChanged(Location location) {
-        boolean positionChanged = false;
-        if (location.getLongitude() != this.longitude){
+        if (this.longitude != location.getLongitude() || this.latitude != location.getLatitude()){
             this.longitude = location.getLongitude();
-            positionChanged = true;
-        }
-        if (location.getLatitude() != this.latitude){
             this.latitude = location.getLatitude();
-            positionChanged = true;
-        }
-        if (location.getSpeed() != this.speed){
             this.speed = location.getSpeed();
-        }
 
-        if(positionChanged){
+            Log.d("GPSReceiver", "location has changed, saving.");
             this.saveGPSInformationToFile(location);
-        }
 
-        updateView();
+            updateView();
+        }
     }
 
     @Override
